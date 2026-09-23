eeef7c81ef136d1295f880f9b888ab1c195f790c
aludpasopati-byte <aludpasopati@gmail.com>
2026-09-23 15:51:13 +0700

    first commit

---
+++ praktikum .py
@@ -0,0 +1,25 @@
+grade=int(input("masukkan grade: "))
+
+if grade >= 90:
+    print("Excellent performance")
+elif grade >= 80:
+    print("Very Good performance")
+elif grade >= 70:
+    print("Good performance")
+elif grade >= 60:
+    print("Average performance")
+else:
+    print("Poor performance")
+
+#angka1=int(input("masukkan angka pertama: "))
+#angka2=int(input("masukkan angka kedua: "))
+#angka3=int(input("masukkan angka ketiga: "))
+
+#if angka1 > angka2 and angka1 > angka3:
+    #print("angka pertama adalah yang terbesar")
+#elif angka2 > angka1 and angka2 > angka3:
+    #print("angka kedua adalah yang terbesar")
+#elif angka3 > angka1 and angka3 > angka2:
+    #print("angka ketiga adalah yang terbesar")
+#else:
+    #print("tidak ada angka yang terbesar")
