64221914bc025a4424efa786882f63c4c811ff0f
aludpasopati-byte <aludpasopati@gmail.com>
2026-09-23 16:13:42 +0700

    py.2

--- praktikum .py
+++ praktikum .py
@@ -11,3 +11,16 @@ elif grade >= 60:
 else:
     print("Poor performance")
 
+angka1=int(input("masukkan angka pertama: "))
+angka2=int(input("masukkan angka kedua: "))
+angka3=int(input("masukkan angka ketiga: "))
+
+if angka1 > angka2 and angka1 > angka3:
+    print("angka pertama adalah yang terbesar")
+elif angka2 > angka1 and angka2 > angka3:
+    print("angka kedua adalah yang terbesar")
+elif angka3 > angka1 and angka3 > angka2:
+    print("angka ketiga adalah yang terbesar")
+else:
+    print("tidak ada angka yang terbesar")
+
